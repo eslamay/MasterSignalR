46634c5920a10881d43dccf56b0d31923e7ac9fe
Sherlock <playyala@yahoo.com>
2025-10-04 16:43:38 +0300

    Add project files.

---
+++ MasterSignalR/wwwroot/js/usersCount.js
@@ -0,0 +1,26 @@
+﻿var connectionUsersCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+connectionUsersCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerHTML = value.toString();
+});
+
+connectionUsersCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerHTML = value.toString();
+});
+
+function NewWindowLoadedClient() {
+    connectionUsersCount.send("NewWindowLoaded");
+}
+
+function fullfilled() {
+    console.log("fullfilled");
+    NewWindowLoadedClient();
+}
+
+function rejected() {
+    console.log("rejected");
+}
+
+connectionUsersCount.start().then(fullfilled, rejected);
